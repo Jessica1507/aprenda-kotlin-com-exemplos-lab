f71d088a89c7d6b1c2434bc9e15711b16ff35116
Jessica Melo <141690085+Jessica1507@users.noreply.github.com>
2023-09-10 02:14:18 -0300

    Ajustes nos valores do enum Nivel

--- desafio.kt
+++ desafio.kt
@@ -1,14 +1,39 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel {INICIANTE, INTERMEDIARIO, AVANCADO}
+fun main() {
+      for (nivel in Nivel.values()) println(nivel.toString())
+      println("O segundo nivel e:${Nivel.valueOf("Intermediario")}")
+  }
 
-class Usuario
+class Usuario (val nome: String, var age: Int)
+fun main() {
+    val usuario = Usuario ("Joao", 18)
+}
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60)
+ fun main() {
+
+     val conteudoEducacional = ConteudoEducacional("Principais conceitos em  Kotlin", 60)
+     val conteudoEducacional01 = ConteudoEducacional(" Desenvolvimento Android", 60)
+     val conteudoEducacional02 = ConteudoEducacional("Desafios de codigo",60 )
+
+     println(conteudoEducacional)
+     println(conteudoEducacional01.copy("Desenvolvimento Android"))
+
+ }
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
-    val inscritos = mutableListOf<Usuario>()
+fun main() {
+    val inscritos = mutableListOf<String>("Joao", "Ana","Andre")
+        inscritos.add("Carlos")
+    println(inscritos)
+
+    inscritos.remove("Carlos")
+    println(inscritos)
+}
     
     fun matricular(usuario: Usuario) {
         TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
